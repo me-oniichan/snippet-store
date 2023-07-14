f8438745c11d98b0fd3231ae8a2307d4dfb6bee6
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2023-07-15 00:11:24 +0530

    login comlete

---
+++ oauth/static/oauth/js/login.js
@@ -0,0 +1,27 @@
+let elem = document.getElementById("showpass")
+let passelem = document.getElementById("password")
+let showpass = false
+
+elem.addEventListener("click", ()=>{
+    if(showpass){
+        showpass=false
+        passelem.setAttribute("type", "password");
+        elem.setAttribute("src", "../static/oauth/eye-open.png")
+    }
+    else{
+        showpass=true
+        passelem.setAttribute("type", "text")
+        elem.setAttribute("src", "../static/oauth/eye-closed.png")
+    }
+})
+
+const rotate = () =>{
+    let bg = document.getElementById("bg")
+    let deg = 0
+    setInterval(()=>{
+        deg = (deg+5)%360
+        bg.style = `background: linear-gradient(${deg}deg, rgba(17, 25, 79, 0.91), rgba(173, 32, 78, 0.89));`
+    }, 500)
+}
+
+rotate()
