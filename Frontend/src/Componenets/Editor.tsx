4c0fc8cb0b8e3f4829b55547356f47316b4fecb7
me-oniichan <naveenbhandari150@gmail.com>
2023-08-22 10:16:31 -0700

    added callback feature

--- Frontend/src/Componenets/Editor.tsx
+++ Frontend/src/Componenets/Editor.tsx
@@ -1,29 +1,38 @@
 import { Editor, useMonaco } from "@monaco-editor/react";
 import "../assets/css/editor.css";
-import { useRef } from "react";
+import { useEffect, useRef, useState, forwardRef } from "react";
 import { AiOutlineReload } from "react-icons/ai";
 import { GiStarFormation } from "react-icons/gi";
 
 interface editorProp {
-  readOnly: boolean,
-  children: string,
-  language: string
+  readOnly: boolean;
+  children: string;
+  language: string;
+  callback?: any;
 }
 
-export default function CodeArea(props: editorProp) {
+// export default forwardRef((props: editorProp, ref: any) => {
+export default function CodeArea(props: editorProp){
   const editorRef: any = useRef();
   const monaco = useMonaco();
+
+  const [language, setLanguage] = useState(props.language);
+
+  useEffect(() => {
+    setLanguage(props.language);
+  }, [props.language]);
 
   const mount = (editor: any, monaco: any) => {
     monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
       noSemanticValidation: true,
       noSyntaxValidation: true,
     });
-
+    // if (ref) ref.current = editor;
     editorRef.current = editor;
   };
 
   const changeLang = (element: any) => {
+    setLanguage(element.target.value);
     if (monaco) {
       monaco.editor.setModelLanguage(
         editorRef.current.getModel(),
@@ -32,7 +41,6 @@ export default function CodeArea(props: editorProp) {
     }
   };
 
-  console.log(props.language);
   return (
     <div className="editor">
       <div className="editor-header">
@@ -40,7 +48,7 @@ export default function CodeArea(props: editorProp) {
           className="language"
           id="language"
           onChange={changeLang}
-          value={props.language}
+          value={language}
         >
           <option value="plaintext">plaintext</option>
           <option value="python">python</option>
@@ -49,84 +57,59 @@ export default function CodeArea(props: editorProp) {
           <option value="javascript">javascript</option>
           <option value="java">java</option>
           <option value="c">c</option>
-          <option value="cpp">cpp</option>
-          <option value="abap">abap</option>
-          <option value="apex">apex</option>
-          <option value="azcli">azcli</option>
+          <option value="cpp">c++</option>
           <option value="bat">bat</option>
-          <option value="bicep">bicep</option>
-          <option value="cameligo">cameligo</option>
           <option value="clojure">clojure</option>
           <option value="coffeescript">coffeescript</option>
           <option value="csharp">csharp</option>
           <option value="csp">csp</option>
-          <option value="cypher">cypher</option>
           <option value="dart">dart</option>
           <option value="dockerfile">dockerfile</option>
-          <option value="ecl">ecl</option>
           <option value="elixir">elixir</option>
-          <option value="flow9">flow9</option>
           <option value="fsharp">fsharp</option>
           <option value="go">go</option>
           <option value="graphql">graphql</option>
-          <option value="handlebars">handlebars</option>
           <option value="hcl">hcl</option>
-          <option value="ini">ini</option>
           <option value="julia">julia</option>
           <option value="kotlin">kotlin</option>
           <option value="less">less</option>
-          <option value="lexon">lexon</option>
           <option value="lua">lua</option>
-          <option value="liquid">liquid</option>
-          <option value="m3">m3</option>
           <option value="markdown">markdown</option>
-          <option value="mips">mips</option>
-          <option value="msdax">msdax</option>
           <option value="mysql">mysql</option>
-          <option value="objective">objective-c</option>
+          <option value="objective-c">objective-c</option>
           <option value="pascal">pascal</option>
           <option value="pascaligo">pascaligo</option>
           <option value="perl">perl</option>
           <option value="pgsql">pgsql</option>
           <option value="php">php</option>
-          <option value="pla">pla</option>
-          <option value="postiats">postiats</option>
-          <option value="powerquery">powerquery</option>
           <option value="powershell">powershell</option>
           <option value="proto">proto</option>
           <option value="pug">pug</option>
-          <option value="qsharp">qsharp</option>
           <option value="r">r</option>
-          <option value="razor">razor</option>
-          <option value="redis">redis</option>
-          <option value="redshift">redshift</option>
-          <option value="restructuredtext">restructuredtext</option>
           <option value="ruby">ruby</option>
           <option value="rust">rust</option>
-          <option value="sb">sb</option>
           <option value="scala">scala</option>
-          <option value="scheme">scheme</option>
           <option value="scss">scss</option>
           <option value="shell">shell</option>
-          <option value="sol">sol</option>
-          <option value="aes">aes</option>
-          <option value="sparql">sparql</option>
           <option value="sql">sql</option>
-          <option value="st">st</option>
           <option value="swift">swift</option>
-          <option value="systemverilog">systemverilog</option>
-          <option value="verilog">verilog</option>
-          <option value="tcl">tcl</option>
           <option value="twig">twig</option>
           <option value="typescript">typescript</option>
           <option value="vb">vb</option>
-          <option value="wgsl">wgsl</option>
           <option value="xml">xml</option>
           <option value="yaml">yaml</option>
           <option value="json">json</option>
         </select>
 
         <div className="editor-options">
+          {props.callback ? (
+            <button className="submit" onClick={()=>props.callback(editorRef.current.getValue(), language)}>
+              Submit
+            </button>
+          ) : (
+            ""
+          )}
+
           {!props.readOnly ? (
             <AiOutlineReload
               size={20}
@@ -140,18 +123,23 @@ export default function CodeArea(props: editorProp) {
           ) : (
             ""
           )}
-          <GiStarFormation size={20} className="icon" onClick={()=>{
-             editorRef.current.getAction('editor.action.formatDocument').run();
-          }}/>
+          <GiStarFormation
+            size={20}
+            className="icon"
+            onClick={() => {
+              editorRef.current.getAction("editor.action.formatDocument").run();
+            }}
+          />
         </div>
       </div>
 
       <Editor
         theme="vs-dark"
-        language={props.language}
+        language={language}
         onMount={mount}
         options={{
           readOnly: props.readOnly,
+          domReadOnly: true,
           fontFamily: "Fira Code",
           fontSize: 18,
         }}
@@ -160,4 +148,4 @@ export default function CodeArea(props: editorProp) {
       <div className="editor-footer"></div>
     </div>
   );
-}
+};
